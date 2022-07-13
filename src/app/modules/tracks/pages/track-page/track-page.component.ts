b8626fda69da9b5f5111885f494c1a6b4d8b21c6
Steveen Personal <ss.rodriguez00021@gmail.com>
2022-07-13 08:11:11 -0500

    se elimina maquetación de track-page

--- src/app/modules/tracks/pages/track-page/track-page.component.ts
+++ src/app/modules/tracks/pages/track-page/track-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.models';
 import { TrackService } from '@modules/tracks/services/track.service';
 import { Subscription } from 'rxjs';
-import * as dataRaw from '../../../../data/tracks.json'
+
 
 @Component({
   selector: 'app-track-page',
@@ -20,24 +20,11 @@ export class TrackPageComponent implements OnInit, OnDestroy {
 
 
   ngOnInit(): void {
-    const observer1$ = this.trackService.dataTracksTrending$
-      .subscribe( response => {
-        this.tracksTrending = response;
-        this.tracksRandom = response;
-        console.log(response)
-      })
 
-      const observer2$ = this.trackService.dataTracksRandom$
-      .subscribe( response => {
-        this.tracksRandom = [...this.tracksTrending, ...response]
-        console.log(response)
-      })
-
-    this.listObservers$ = [observer1$, observer2$]
   }
 
   ngOnDestroy(): void {
-    this.listObservers$.forEach(observer => observer.unsubscribe());
+
   }
 
 }
