829ba027dee7bdb7ad06576f138fb498a0b1bb5a
Steveen Personal <ss.rodriguez00021@gmail.com>
2022-07-13 10:50:59 -0500

    se aplica pipe para filtrar por Id

--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { TrackModel } from '@core/models/tracks.models';
+import { map, Observable, mergeMap, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 
@@ -17,11 +18,45 @@ export class TrackService {
 
   }
 
-  getAllTracks$(): Observable<any> {
-    // Traemos todas las canciones con el método Get()
-    return this.httpClient.get(`${this.URL}/tracks`)
+  // función que permite filtrar por el id que le pasemos y no mostrarlo en la lista
+    // recibe dos parámetros de tipo  TrackModel[] y id:number
+  private skipById(listTracks:TrackModel[], id:number):
+  Promise<TrackModel[]> {
+    return new Promise((resolve, reject) => {
+      // lista temporal aplicando el filtro
+      const listTemp = listTracks.filter(a => a._id !== id)
+      resolve(listTemp)
+    })
   }
 
 
+  // este método retorna un observable de tipo HttpClien
+  // los observables le podemos hacer uso de (pipe)
+  getAllTracks$(): Observable<any> {
+    // Traemos todas las canciones con el método Get()
+    return this.httpClient.get(`${this.URL}/tracks`)
+      .pipe(
+        // utilizando destructuración llamamos la propiedad data
+        map(({data}:any) => {
+          return data
+        })
+      )
+  }
+
+
+  getAllRandom$(): Observable<any> {
+    // Traemos todas las canciones con el método Get()
+    return this.httpClient.get(`${this.URL}/tracks`)
+      .pipe(
+        // utilizando destructuración llamamos la propiedad data
+        mergeMap(({data}:any) => this.skipById(data, 1)),
+        // map((dataRevertida) => {
+        //   // devolvemos la data con el filtro que le apliquemos
+        //   return dataRevertida.filter((track: TrackModel) => track._id !== 1)
+        // })
+        tap(data  => console.log('✔✔✔✔',data))
+
+      )
+  }
 
 }
