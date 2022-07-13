0a8ea1cc324c5e079114b82a98c9e3a9d605d14a
Steveen Personal <ss.rodriguez00021@gmail.com>
2022-07-13 08:23:58 -0500

    se agrega HttpClientModule en el trackModule

--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.models';
 import { observable, Observable, of } from 'rxjs';
-import * as dataRaw from '../../../data/tracks.json'
 
 
 @Injectable({
@@ -14,25 +13,6 @@ export class TrackService {
 
 
   constructor() {
-    const { data }: any = (dataRaw as any).default;
-    this.dataTracksTrending$ = of(data)
-
-    this.dataTracksRandom$ = new Observable((observer) => {
-
-      const trackExample: TrackModel = {
-        _id: 9,
-        name: 'level',
-        album: 'dj',
-        url: 'http',
-        cover: 'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg'
-      }
-
-
-      setTimeout(()=> {
-        observer.next([trackExample])
-      }, 1500)
-
-
-    })
+    
   }
 }
